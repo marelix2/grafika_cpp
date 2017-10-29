dfaadbf5b0f0131430e3d8152c0bb2d7be7a44e5
marelix2 <marelix2@gmail.com>
2017-10-29 23:19:46 +0100

    lab3

--- lab3/zad.cpp
+++ lab3/zad.cpp
@@ -8,7 +8,7 @@
 #include <stack>
 
 
-#define M_PI 3.14159265358979323846264338327
+#define M_PI 3.14159265358979323846264338327 / 4.0
 
 
 int screenWidth = 800;
@@ -77,8 +77,9 @@ drawCircle(makecol(255,255,255));
 void drawCircle(int color){
     int x1, y1;
     double i;
+    int RR = r*2;
 
-   for( i = 0; i <= M_PI/4.0; i += 1.0 / (2 * r)){
+   for( i = 0; i <= M_PI; i += 1.0 / RR){
         x1 = r * cos(i);
         y1 = r * sin(i);
         putpixel(screen, x + x1, y + y1, color);
@@ -96,8 +97,9 @@ void drawCircle(int color){
 void drawCircle(int color, int R){
     int x1, y1;
     double i;
+    int RR=R*2;
 
-   for( i = 0; i <= 3.14/4.0; i += 1.0 / (2 * r)){
+   for( i = 0; i <= M_PI+1; i += 1.0 / RR){
         x1 = R * cos(i);
         y1 = R * sin(i);
         putpixel(screen, x + x1, y + y1, color);
@@ -125,19 +127,166 @@ void fillBackground(){
     for(int i = 0; i< r ; i++){
 
         drawCircle(filingColor, i);
+    }
+
+}
+
+};
 
 
+int main(){
+    allegro_init();
+    set_gfx_mode(GFX_AUTODETECT_WINDOWED, screenWidth, screenHeight, 0, 0);
+    install_keyboard();
+    install_mouse();
+    install_timer();
+
+    Circle *cc;
+
+
+    while(1){
+            cc = new Circle;
+            cc->initDraw();
+            cc->fillBackground();
 
 
     }
+    return 0;
+}
+END_OF_MAIN();
+
+#include <allegro.h>
+#include <time.h>
+#include <winalleg.h>
+#include <cstdio>
+#include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stack>
 
 
+#define M_PI 3.14159265358979323846264338327 / 4.0
 
+
+int screenWidth = 800;
+int screenHeight = 640;
+
+
+class Circle{
+
+int    x;
+int    y;
+int    r;
+
+BITMAP * _bitmap;
+
+public :
+Circle(int x, int y, int r){
+
+this->x = x;
+this->y = y;
+this->r = r;
 
 }
 
 
+Circle(){
 
+this->x = randomX();
+this->y = randomY();
+this->r = randomR();
+
+}
+int randomX(){
+
+return  rand()%screenWidth;
+
+}
+
+int randomY(){
+
+return  rand()%screenHeight;
+
+}
+
+int randomR(){
+
+return 20 +rand()%50;
+
+}
+
+int getX() {
+            return x;
+        }
+
+        int getY() {
+            return y;
+        }
+
+
+void initDraw(){
+
+
+drawCircle(makecol(255,255,255));
+
+}
+
+void drawCircle(int color){
+    int x1, y1;
+    double i;
+    int RR = r*2;
+
+   for( i = 0; i <= M_PI; i += 1.0 / RR){
+        x1 = r * cos(i);
+        y1 = r * sin(i);
+        putpixel(screen, x + x1, y + y1, color);
+        putpixel(screen, x + y1, y + x1, color);
+        putpixel(screen, x - y1, y + x1, color);
+        putpixel(screen, x - x1, y + y1, color);
+        putpixel(screen, x - x1, y - y1, color);
+        putpixel(screen, x - y1, y - x1, color);
+        putpixel(screen, x + y1, y - x1, color);
+        putpixel(screen, x + x1, y - y1, color);
+    }
+
+};
+
+void drawCircle(int color, int R){
+    int x1, y1;
+    double i;
+    int RR=R*2;
+
+   for( i = 0; i <= M_PI+1; i += 1.0 / RR){
+        x1 = R * cos(i);
+        y1 = R * sin(i);
+        putpixel(screen, x + x1, y + y1, color);
+        putpixel(screen, x + y1, y + x1, color);
+        putpixel(screen, x - y1, y + x1, color);
+        putpixel(screen, x - x1, y + y1, color);
+        putpixel(screen, x - x1, y - y1, color);
+        putpixel(screen, x - y1, y - x1, color);
+        putpixel(screen, x + y1, y - x1, color);
+        putpixel(screen, x + x1, y - y1, color);
+    }
+
+};
+
+int randomColor(){
+
+return makecol(rand()%256,rand()%256,rand()%256);
+
+}
+
+void fillBackground(){
+
+    int filingColor = randomColor();
+
+    for(int i = 0; i< r ; i++){
+
+        drawCircle(filingColor, i);
+    }
+
+}
 
 };
 
